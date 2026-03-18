14932299ac366e6d33a8ac8570baf21647596b7f
DANIEL LAU PEREIRA SOARES <danniellau@gmail.com>
2026-03-17 21:07:25 -0300

    fix: corrige test_helper.exs (remove linha invalida, garante migrations)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,9 +1,5 @@
 ExUnit.start()
 
 # Garante que as migrations foram aplicadas no banco de testes
-Alias = UncoverAegis.Repo
-Ecto.Adapters.SQL.Sandbox
-
-# Roda migrations antes da suite de testes
 {:ok, _} = Application.ensure_all_started(:uncover_aegis)
 Ecto.Migrator.run(UncoverAegis.Repo, :up, all: true, log: false)
